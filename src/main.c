ff7d49d73619a15d3272b552284073ff39408a93
Robbert Gurdeep Singh <git@beardhatcode.be>
2020-10-01 10:17:20 +0200

    Initial boilerplate

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+   printf("Project DA3!");
+   return 0;
+}
---
+++ tests/runTests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo "Starting to run tests"
+
+echo "Compiling..."
+
+echo "Running tests"
+
+echo "No tests run"
+exit 1
