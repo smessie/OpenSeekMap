1073de52bbe532a730d491e53df138255fcff4a4
smessie <smessie@smessie.com>
2020-11-28 20:45:45 +0100

    Stop early if cost already higher than max

--- src/algorithm2.c
+++ src/algorithm2.c
@@ -13,6 +13,7 @@ int edit_distance(uint32_t* z, uint32_t* t, int length_z, int length_t) {
         d[0][j] = j;
     }
     for (int i = 1; i <= length_z; i++) {
+        int min_value = 4; // At least one higher than max cost (3).
         for (int j = 1; j <= length_t; j++) {
             int exchange_cost = 1;
             if (z[i - 1] == t[j - 1]) {
@@ -21,7 +22,15 @@ int edit_distance(uint32_t* z, uint32_t* t, int length_z, int length_t) {
             if (z[i - 1] == t[j] && z[i] == t[j - 1]) {
                 exchange_cost = 0;
             }
-            d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + exchange_cost);
+            int cost = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + exchange_cost);
+            d[i][j] = cost;
+            if (cost < min_value) {
+                min_value = cost;
+            }
+        }
+        if (min_value > 3) {
+            // If our smallest value is already bigger than our max cost, we'll never find an edit distance smaller than or equal to max cost.
+            return -1;
         }
     }
     return d[length_z][length_t];
